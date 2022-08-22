eec548889da101e24293fa3fb1a51cba131b532e
rudransh-singh <rudransh.s@research.iiit.ac.in>
2022-08-23 02:49:14 +0530

    plotly stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import Plot from 'react-plotly.js';
+var xlist = [0];
+var ylist = [0];
+function App () {
+  const [settings, setSettings] = useState(0);
+  useEffect(() => {
+  setInterval(() => {
+    console.log("changing xlist and ylist");
+    // choose a random number between 0 and 100
+    const y = Math.random() * 100;
+    ylist = [...ylist, y];
+    xlist = [...xlist, xlist.length];
+    console.log("xlist is", xlist);
+    console.log("ylist is", ylist);
+    setSettings(settings => settings+1);
+  }, 1000);
+  }, []);
+      
+    
 
-class App extends React.Component {
-  render() {
-    return (
-      <div id="ESW_Dashboard">
-        Hello World
+  return (
+    <div className="App">
       <Plot
         data={[
           {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
+            x: xlist,
+            y: ylist,
             type: 'scatter',
             mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'line', x: [1, 2, 3], y: [2, 5, 3]},
-          {type: 'line', x: [1, 2, 3], y: [2, 7, 3]},
+            marker: {
+              color: 'red',
+              size: 12
+            }
+          }
         ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+        layout={ {width: 400, height: 400} }
       />
-      </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default App;
