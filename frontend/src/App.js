41d32901364b6b5a8177c7f49075e6d2eab26313
rudransh-singh <rudransh.s@research.iiit.ac.in>
2022-08-23 02:56:00 +0530

    updated code to shift plot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import Plot from 'react-plotly.js';
 var xlist = [0];
 var ylist = [0];
+var length = 0;
 function App () {
   const [settings, setSettings] = useState(0);
   useEffect(() => {
@@ -10,7 +11,13 @@ function App () {
     // choose a random number between 0 and 100
     const y = Math.random() * 100;
     ylist = [...ylist, y];
-    xlist = [...xlist, xlist.length];
+    length++;
+    xlist = [...xlist, length];
+
+    if(ylist.length > 10){
+      ylist.shift();
+      xlist.shift();
+    }
     console.log("xlist is", xlist);
     console.log("ylist is", ylist);
     setSettings(settings => settings+1);
@@ -21,6 +28,7 @@ function App () {
 
   return (
     <div className="App">
+      settings : {settings}
       <Plot
         data={[
           {
