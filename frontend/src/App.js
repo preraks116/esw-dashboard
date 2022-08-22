e6368d4d98fcc995582b52c6ce7779eeee3c79ab
rudransh-singh <rudransh.s@research.iiit.ac.in>
2022-08-23 04:47:46 +0530

    fixed zoom issues :

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,35 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
 var xlist = [0];
 var ylist = [0];
 var length = 0;
-function App () {
+function App() {
   const [settings, setSettings] = useState(0);
   useEffect(() => {
-  setInterval(() => {
-    console.log("changing xlist and ylist");
-    // choose a random number between 0 and 100
-    const y = Math.random() * 100;
-    ylist = [...ylist, y];
-    length++;
-    xlist = [...xlist, length];
+    setInterval(() => {
+      console.log("changing xlist and ylist");
+      // choose a random number between 0 and 100
+      const y = Math.random() * 100;
+      ylist = [...ylist, y];
+      length++;
+      xlist = [...xlist, length];
 
-    if(ylist.length > 10){
-      ylist.shift();
-      xlist.shift();
-    }
-    console.log("xlist is", xlist);
-    console.log("ylist is", ylist);
-    setSettings(settings => settings+1);
-  }, 1000);
+      if (ylist.length > 10) {
+        ylist.shift();
+        xlist.shift();
+      }
+      console.log("xlist is", xlist);
+      console.log("ylist is", ylist);
+      setSettings(settings => settings + 1);
+    }, 1000);
   }, []);
-      
-    
+
+
 
   return (
     <div className="App">
-      settings : {settings}
+      <h1>ESW DashBoard</h1>
+      <div id='graph'>
       <Plot
         data={[
           {
@@ -42,8 +43,23 @@ function App () {
             }
           }
         ]}
-        layout={ {width: 400, height: 400} }
+        layout={{
+          width: 1000,
+          height: 400,
+          autosize: true,
+          l: 0, r: 0, b: 0, t: 0, pad: 0,
+          showlegend: false,
+          paper_bgcolor: 'rgba(0,0,0,0)',
+          plot_bgcolor: 'rgba(0,0,0,0)',
+          xaxis: {
+            uirevision: 'time',
+          },
+          yaxis: {
+            uirevision: 'time',
+          },
+        }}
       />
+      </div>
     </div>
   );
 }
