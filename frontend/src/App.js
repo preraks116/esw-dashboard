be04a44dfc3b5c1827f7bd8065486bdd221362e1
rudransh-singh <rudransh.s@research.iiit.ac.in>
2022-09-03 01:58:21 +0530

    added ThingSpeak

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, startTransition } from 'react';
 import Graph from './Graph';
 import styles from './style.module.css';
-
+import axios from 'axios';
 // make data cl
+const channelid = "1848239";
+const readAPIKey = "XVYQYXZQYXZQYXZQ";
+const stats = {
+  lastTime: "",
+  rps: {
+    val: [],
+    xlist: [],
+    measure: {
+      mean: 0,
+      median: 0,
+      std: 0
+    },
+  },
+  voltage: {
+    val: [],
+    xlist: [],
+    measure: {
+      mean: 0,
+      median: 0,
+      std: 0
+    },
+  },
+  dutyCycle: {
+    val: [],
+    xlist: [],
+    measure: {
+      mean: 0,
+      median: 0,
+      std: 0
+    },
+  }
+}
 
 const rpm = {
   xlist: [0],
   ylist: [0],
-  length : 0,
+  length: 0,
   stats: {
     mean: 0,
     median: 0,
@@ -18,7 +50,7 @@ const rpm = {
 const voltage = {
   xlist: [0],
   ylist: [0],
-  length : 0,
+  length: 0,
   stats: {
     mean: 0,
     median: 0,
@@ -52,15 +84,86 @@ function addValue(data) {
   data.xlist = [...data.xlist, data.length];
 }
 
+function addStats(data, field) {
+  if (field === "rps") {
+    stats.rps.val = [...stats.rps.val, data];
+    stats.rps.xlist = [...stats.rps.xlist, stats.rps.val.length];
+    const rpslength = stats.rps.val.length;
+    console.log("rpslength is", rpslength);
+    console.log("sum of all elements in the array is",stats.rps.val.reduce((a, b) => a + b));
+    const mean = stats.rps.val.reduce((a, b) => a + b) / stats.rps.val.length;
+    console.log("mean is", mean);
+    const median = stats.rps.val[Math.floor(rpslength / 2)];
+    const std = Math.sqrt(stats.rps.val.reduce((a, b) => a + Math.pow(b - mean, 2)) / rpslength);
+    stats.rps.measure = {
+      mean: mean,
+      median: median,
+      std: std
+    }
+
+  }
+  if (field === "voltage") {
+    stats.voltage.val = [...stats.voltage.val, data];
+    stats.voltage.xlist = [...stats.voltage.xlist, stats.voltage.val.length];
+    const voltagelength = stats.voltage.val.length;
+    const mean = stats.voltage.val.reduce((a, b) => a + b) / stats.voltage.val.length;
+    const median = stats.voltage.val[Math.floor(voltagelength / 2)];
+    const std = Math.sqrt(stats.voltage.val.reduce((a, b) => a + Math.pow(b - mean, 2)) / voltagelength);
+    stats.voltage.measure = {
+      mean: mean,
+      median: median,
+      std: std
+    }
+  }
+  if (field === "dutyCycle") {
+    stats.dutyCycle.val = [...stats.dutyCycle.val, data];
+    stats.dutyCycle.xlist = [...stats.dutyCycle.xlist, stats.dutyCycle.val.length];
+    const dutyCyclelength = stats.dutyCycle.val.length;
+    const mean = stats.dutyCycle.val.reduce((a, b) => a + b) / stats.dutyCycle.val.length;
+    const median = stats.dutyCycle.val[Math.floor(dutyCyclelength / 2)];
+    const std = Math.sqrt(stats.dutyCycle.val.reduce((a, b) => a + Math.pow(b - mean, 2)) / dutyCyclelength);
+    stats.dutyCycle.measure = {
+      mean: mean,
+      median: median,
+      std: std
+    }
+  }
+}
+
 function App() {
   const [settings, setSettings] = useState(0);
   const [voltageValue, setVoltage] = useState(5);
+
+  useEffect(() => {
+    //fetch from the thingspeak API every 10 secs
+    setInterval(() => {
+      axios.get(`https://api.thingspeak.com/channels/${channelid}/feed/last.json`)
+        .then((res) => {
+          const rpm = Number(res.data.field1);
+          const voltage = Number(res.data.field2);
+          const dutyCycle = Number(res.data.field3);
+          const lastTime = res.data.created_at;
+          if (lastTime !== stats.lastTime) {
+            addStats(rpm, "rps");
+            addStats(voltage, "voltage");
+            addStats(dutyCycle, "dutyCycle");
+            stats.lastTime = lastTime;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        }
+        )
+    }, 1000);
+
+  }, []);
+
+
   useEffect(() => {
     setInterval(() => {
       addValue(rpm);
       getStats(rpm);
       // add random value between -2 and 2 to voltage
-      addVoltage(voltageValue + Math.random() * 4 - 2);
       getStats(voltage);
       // console.log(voltage);
       // console.log(voltageValue)
@@ -73,38 +176,50 @@ function App() {
   return (
     <div className="App">
       <h1 className={styles.bigblue}>ESW DashBoard</h1>
-      <div id='graph' className={styles.graph}>
+      <div id='graphrps' className={styles.graph}>
         <Graph
-          xlist={rpm.xlist}
-          ylist={rpm.ylist}
+          xlist={stats.rps.xlist}
+          ylist={stats.rps.val}
           title='RPM'
         />
         <div className={styles.stats}>
           <ul>
-            <li>Mean: {rpm.stats.mean.toFixed(2)}</li>
-            <li>Median: {rpm.stats.median.toFixed(2)}</li>
-            <li>Std: {rpm.stats.std.toFixed(2)}</li>
+            <li>Mean: {stats.rps.measure.mean.toFixed(2)}</li>
+            <li>Median: {stats.rps.measure.median.toFixed(2)}</li>
+            <li>Std: {stats.rps.measure.std.toFixed(2)}</li>
           </ul>
         </div>
       </div>
-      <div id='graph' className={styles.graph}>
+      <div id='graphvolt' className={styles.graph}>
         <Graph
-          xlist={voltage.xlist}
-          ylist={voltage.ylist}
+          xlist={stats.voltage.xlist}
+          ylist={stats.voltage.val}
           title='Voltage'
         />
         <div className={styles.stats}>
           <ul>
-            <li>Mean: {voltage.stats.mean.toFixed(2)}</li>
-            <li>Median: {voltage.stats.median.toFixed(2)}</li>
-            <li>Std: {voltage.stats.std.toFixed(2)}</li>
+            <li>Mean: {stats.voltage.measure.mean.toFixed(2)}</li>
+            <li>Median: {stats.voltage.measure.median.toFixed(2)}</li>
+            <li>Std: {stats.voltage.measure.std.toFixed(2)}</li>
           </ul>
         </div>
-        {/* <div className={styles.slider}>
-          <input type="range" min="0" max="100" value={voltageValue} onChange={(e) => setVoltage(e.target.value)} />
-        </div> */}
+      </div>
+      <div id='graphdutyCycle' className={styles.graph}>
+        <Graph
+          xlist={stats.dutyCycle.xlist}
+          ylist={stats.dutyCycle.val}
+          title='dutyCycle'
+        />
+        <div className={styles.stats}>
+          <ul>
+            <li>Mean: {stats.dutyCycle.measure.mean.toFixed(2)}</li>
+            <li>Median: {stats.dutyCycle.measure.median.toFixed(2)}</li>
+            <li>Std: {stats.dutyCycle.measure.std.toFixed(2)}</li>
+          </ul>
+        </div>
       </div>
     </div>
+
   );
 }
 
